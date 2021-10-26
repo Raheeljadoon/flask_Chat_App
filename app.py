16370eb6d5ef4e6371a54f7c62d78cbffe4c6c4b
raheeljadoon56 <raheel.raiz@rapidev.ae>
2021-10-26 16:56:41 +0500

    database and login added

--- app.py
+++ app.py
@@ -1,14 +1,34 @@
 from flask import Flask, render_template ,request, redirect , url_for 
 from flask_socketio import SocketIO , join_room
+from flask_login import LoginManager, login_required, login_user,logout_user
+from db import get_user
 
 app = Flask(__name__)
 socketio = SocketIO(app)
+login_manager = LoginManager()
+login_manager.init_app(app) 
 
 @app.route("/")
 def home():
     # return "<p>Hello, World!</p>"
     title = "code title"
     return render_template("index.html", title = title)
+
+@app.route("/login", methods = ["GET","POST"])
+def login():
+    message = ''
+    if request.method == "POST":
+        username = request.form.get('username')
+        password_input = request.form.get('password')
+        user = get_user('username')
+        print(user)
+        if user and user.check_password(password_input):
+            login_user(user)
+            return redirect(url_for("home"))
+        else :
+            message = "failed to login"
+
+    return render_template('login.html', message = message)
 
 @app.route("/chat")
 def chat():
@@ -32,6 +52,11 @@ def handle_join_room_event(data):
     app.logger.info("{} has join the room {}".format(data['username'],data['room']))
     join_room(data['room'])
     socketio.emit('join_room_annoucement', data)
+
+@login_manager.user_loader
+def load_user(username):
+    return get_user(username)
+
 
 if __name__ == "__main__":
     socketio.run(app ,debug=True)
---
+++ db.py
@@ -0,0 +1,22 @@
+from pymongo import MongoClient
+from werkzeug.security import generate_password_hash
+from  user import User
+
+client = MongoClient('mongodb://root:pass12345@localhost:27017/')
+
+chat_db = client.get_database('ChatDB')
+users_collection = chat_db.get_collection('users')
+
+def save_user(username , email , password):
+    password_hash = generate_password_hash(password)
+    users_collection.insert_one({'_id' : username,'email' : email , 'password': password_hash})
+
+
+def get_user(username):
+    user_data = users_collection.find_one({"id": username})
+    print("---------user_data----------",user_data)
+    return User (user_data['id'],user_data['email'],user_data['password']) if user_data else None
+
+
+# save_user("oykamal","kamal@gmail.com","pass")
+print(get_user('oykamal'))
